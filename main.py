ca31a2c8fb5dedb7e9808d54b674936227bf3f01
KarenMoreno06 <karen.moreno35879@ucaldas.edu.co>
2024-11-17 22:22:16 -0500

    feat:conetion

--- main.py
+++ main.py
@@ -5,72 +5,69 @@ from dotenv import load_dotenv
 from epaycosdk.epayco import Epayco
 import requests
 
+load_dotenv()
 
-load_dotenv()#carga las variables de entorno que estan en el archivo .env
+app = Flask(__name__)
 
-app = Flask(__name__) # Esta instancia se utiliza para configurar y ejecutar la aplicación web
-
-# Instancia la clase Epayco con las credenciales de la cuenta
+# Instancia Epayco
 epayco = Epayco({
     'apiKey': os.getenv('EPAYCO_PUBLIC_KEY'),
     'privateKey': os.getenv('EPAYCO_PRIVATE_KEY'),
-    'lenguage': 'ES', #lenjuage de los mensajes
-    'test': os.getenv('EPAYCO_TEST') == 'true', #"Aqui hay un cambio" "#definir de modo de prueba a produccion
-
+    'lenguage': 'ES',
+    'test': os.getenv('EPAYCO_TEST') == 'true',
 })
 
+# Método para verificar la existencia de invoice_reference en otro microservicio
+def invoice_reference(invoice_reference):
+    try:
+        # Validar la referencia de la factura con el microservicio de negocio
+        url = f"{os.getenv('MS_BUSINESS_URL')}/invoices/{invoice_reference}"
+        response = requests.get(url)
+        
+        if response.status_code == 200 and response.json().get('invoice'):
+            return response.json().get('invoice_reference')
+        else:
+            return {'error': 'La referencia de la factura no es válida o no existe.'}
+    except Exception as e:
+        return {'error': f'Error de conexión: {str(e)}'}
 
-#metodo para el token de la tarjeta
+
+# Método para el token de la tarjeta
 def create_token(data):
     try:
         card_info = {
             "card[number]": data['card_number'],
-            "card[exp_year]":data['exp_year'],
+            "card[exp_year]": data['exp_year'],
             "card[exp_month]": data['exp_month'],
             "card[cvc]": data['cvc'],
-            "hasCvv": True  # hasCvv: validar codigo de seguridad en la transacción
+            "hasCvv": True
         }
-        token = epayco.token.create(card_info)
-        return token
+        return epayco.token.create(card_info)
     except Exception as e:
         return {'error': str(e)}
 
-    #metodo para crear un cliente
-def create_customer(token,data):
-    customer_info={
+# Método para crear un cliente
+def create_customer(token, data):
+    customer_info = {
         'name': data['name'],
-        'last_name':data['last_name'],
-        'email':data['email'],
-        'phone' : data['phone'],
-        'default': True
+        'last_name': data['last_name'],
+        'email': data['email'],
+        'phone': data['phone'],
+        'default': True,
+        'token_card': token
     }
-    customer_info['token_card'] = token
     try:
-        customer = epayco.customer.create(customer_info)
-        return customer
+        return epayco.customer.create(customer_info)
     except Exception as e:
         return {'error': str(e)}
-    
-    # Método para obtener la invoice_reference desde el microservicio de negocio
-def get_invoice_reference(invoice_reference):
-    try:
-        # Hacer una solicitud HTTP al microservicio para obtener la referencia de la factura
-        url = f"{os.getenv('MS_BUSINESS_URL')}/createMSP/{id}"
-        response = requests.get(url)
-        
-        if response.status_code == 200:
-            return response.json().get('invoice_reference')
-        else:
-            return None
-    except Exception as e:
-        return None
 
+# Método para procesar el pago
 def process_payment(data, customer_id, token_card):
     try:
         payment_info = {
             'token_card': token_card,
             'customer_id': customer_id,
-            "doc_type": "CC",
+            'doc_type': "CC",
             'doc_number': data['doc_number'],
             'name': data['name'],
             'last_name': data['last_name'],
@@ -85,71 +82,54 @@ def process_payment(data, customer_id, token_card):
             'tax': '0',
             'tax_base': data['value'],
             'currency': 'COP',
-            'invoice_reference': data['invoice_reference'] 
+            'invoice_reference': data['invoice_reference']
         }
-       
-        response = epayco.charge.create(payment_info)
-        print("Payment Response:", json.dumps(response, indent=4))  # Agregar detalles para debug
-
-        # Validar el estado de la transacción
-        if response.get('status') is True and response.get('data', {}).get('estado') == 'Aceptada':
-            return {
-                'status': 'success',
-                'message': 'Pago realizado con éxito',
-                'transaction_id': response['data']['ref_payco'],
-                'details': response['data']
-            }
-        else:
-            return {
-                'status': 'failed',
-                'message': 'Pago fallido',
-                'error': response.get('data', {}).get('description', 'No se pudo procesar el pago'),
-                'details': response['data']
-            }
-
+        return epayco.charge.create(payment_info)
     except Exception as e:
         return {'error': str(e)}
 
-
-
-#enpoint para manejar todo el flujo de pago
-@app.route('/proces_payment', methods=['POST'])
+# Endpoint para manejar todo el flujo de pago
+@app.route('/process_payment', methods=['POST'])
 def handle_process_payment():
     data = request.json
 
-#crea el token de la tarjeta
+    # Verificar la referencia de la factura
+    invoice_ref_check = invoice_reference(data['invoice_reference'])
+
+    # Validar si la referencia es None
+    if invoice_ref_check is None or 'error' in invoice_ref_check:
+        error_message = invoice_ref_check if invoice_ref_check else {'error': 'No se obtuvo respuesta del microservicio de negocio'}
+        return jsonify(error_message), 400
+
+    # Crear token de tarjeta
     token_response = create_token(data)
-    print("Token response", json.dumps(token_response))
+    if token_response.get("status") is False:
+        return jsonify({'error': 'Error al crear token', 'details': token_response}), 500
 
-#verificar si hubo error al crear el token
-    if token_response["status"] is False:
-        return jsonify(token_response), 500
+    token_card = token_response['id']
 
-    token_card = token_response['id'] #extrae el id del token
-
-    #Crear cliente
+    # Crear cliente
     customer_response = create_customer(token_card, data)
-    print("Customer response", json.dumps(customer_response))
-
-    #Verificar si hubo error al crear el cliente
     if 'error' in customer_response:
-        return jsonify(customer_response), 500
+        return jsonify({'error': 'Error al crear cliente', 'details': customer_response}), 500
 
     customer_id = customer_response['data']['customerId']
 
-    #Procesar el pago
+    # Procesar pago
     payment_response = process_payment(data, customer_id, token_card)
-    print("Payment Response:", json.dumps(payment_response, indent=4))
-
-    if payment_response.get('status') == 'success':
-        return jsonify(payment_response), 200
+    if payment_response.get('status') is True and payment_response.get('data', {}).get('estado') == 'Aceptada':
+        return jsonify({
+            'status': 'success',
+            'message': 'Pago realizado con éxito',
+            'transaction_id': payment_response['data']['ref_payco'],
+            'details': payment_response['data']
+        }), 200
     else:
         return jsonify({
-        'status': 'error',
-        'message': 'Hubo un problema al procesar el pago',
-        'details': payment_response
-    }), 500
-
+            'status': 'error',
+            'message': 'No se pudo procesar el pago',
+            'details': payment_response
+        }), 500
 
 
 if __name__ == '__main__':
